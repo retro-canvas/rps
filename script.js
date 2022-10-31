ff695a8acb7ae9554ffa1c1a1833978662a3c23a
retro-canvas <mahdigh@live.com>
2022-10-31 12:20:36 +0330

    connected logic and GUI through DOM

--- script.js
+++ script.js
@@ -2,45 +2,58 @@ choiceYours = (arg) => {
   console.log(`your choice is ${arg}`);
   let playerChoice = arg;
   let pcsChoice = Math.floor(Math.random() * 3);
-
   const choices = ["rock", "paper", "scissors"];
+
+  document.getElementById("pc-choice").innerHTML = `${choices[pcsChoice]}`;
 
   // 0 for rock , 1 for paper , 2 for scissors
   // PLAYER CHOOSE ROCK
   if (playerChoice == "rock" && pcsChoice == 0) {
-    console.log(`PC chooses ${choices[pcsChoice]}`);
-    console.log("TIE!");
+    // console.log(`PC chooses ${choices[pcsChoice]}`);
+    // console.log("TIE!");
+    var resultA = "TIE!";
   } else if (playerChoice == "rock" && pcsChoice == 1) {
-    console.log(`PC chooses ${choices[pcsChoice]}`);
-    console.log("You lose!");
+    // console.log(`PC chooses ${choices[pcsChoice]}`);
+    // console.log("You lose!");
+    var resultA = "You lose!";
   } else if (playerChoice == "rock" && pcsChoice == 2) {
-    console.log(`PC chooses ${choices[pcsChoice]}`);
-    console.log("You win!");
+    // console.log(`PC chooses ${choices[pcsChoice]}`);
+    // console.log("You win!");
+    var resultA = "You win!";
   }
   // PLAYER CHOOSE PAPER
   if (playerChoice == "paper" && pcsChoice == 0) {
-    console.log(`PC chooses ${choices[pcsChoice]}`);
-    console.log("You win!");
+    // console.log(`PC chooses ${choices[pcsChoice]}`);
+    // console.log("You win!");
+    var resultA = "You win!";
   } else if (playerChoice == "paper" && pcsChoice == 1) {
-    console.log(`PC chooses ${choices[pcsChoice]}`);
-    console.log("TIE!");
+    // console.log(`PC chooses ${choices[pcsChoice]}`);
+    // console.log("TIE!");
+    var resultA = "TIE!";
   } else if (playerChoice == "paper" && pcsChoice == 2) {
-    console.log(`PC chooses ${choices[pcsChoice]}`);
-    console.log("You lose!");
+    // console.log(`PC chooses ${choices[pcsChoice]}`);
+    // console.log("You lose!");
+    var resultA = "You lose!";
   }
   // PLAYER CHOOSE SCISSOR
   if (playerChoice == "scissor" && pcsChoice == 0) {
-    console.log(`PC chooses ${choices[pcsChoice]}`);
-    console.log("You lose!");
+    // console.log(`PC chooses ${choices[pcsChoice]}`);
+    // console.log("You lose!");
+    var resultA = "You lose!";
   } else if (playerChoice == "scissor" && pcsChoice == 1) {
-    console.log(`PC chooses ${choices[pcsChoice]}`);
-    console.log("you win!");
+    // console.log(`PC chooses ${choices[pcsChoice]}`);
+    // console.log("you win!");
+    var resultA = "You win!";
   } else if (playerChoice == "scissor" && pcsChoice == 2) {
-    console.log(`PC chooses ${choices[pcsChoice]}`);
-    console.log("TIE!");
+    // console.log(`PC chooses ${choices[pcsChoice]}`);
+    // console.log("TIE!");
+    var resultA = "TIE!";
   } else {
+    // ???
     console.log("nothing.");
   }
+  // DECLARING RESULT
+  document.getElementById("result").innerHTML = resultA;
 };
 
 const gameElement0 = document.getElementById("rockBtn");

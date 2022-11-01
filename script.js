418af88f4bb3d5f7ac612333a0ec4811ae7acf90
retro-canvas <mahdigh@live.com>
2022-11-01 10:39:20 +0330

    changed the theme

--- script.js
+++ script.js
@@ -9,44 +9,26 @@ choiceYours = (arg) => {
   // 0 for rock , 1 for paper , 2 for scissors
   // PLAYER CHOOSE ROCK
   if (playerChoice == "rock" && pcsChoice == 0) {
-    // console.log(`PC chooses ${choices[pcsChoice]}`);
-    // console.log("TIE!");
     var resultA = "TIE!";
   } else if (playerChoice == "rock" && pcsChoice == 1) {
-    // console.log(`PC chooses ${choices[pcsChoice]}`);
-    // console.log("You lose!");
     var resultA = "You lose!";
   } else if (playerChoice == "rock" && pcsChoice == 2) {
-    // console.log(`PC chooses ${choices[pcsChoice]}`);
-    // console.log("You win!");
     var resultA = "You win!";
   }
   // PLAYER CHOOSE PAPER
   if (playerChoice == "paper" && pcsChoice == 0) {
-    // console.log(`PC chooses ${choices[pcsChoice]}`);
-    // console.log("You win!");
     var resultA = "You win!";
   } else if (playerChoice == "paper" && pcsChoice == 1) {
-    // console.log(`PC chooses ${choices[pcsChoice]}`);
-    // console.log("TIE!");
     var resultA = "TIE!";
   } else if (playerChoice == "paper" && pcsChoice == 2) {
-    // console.log(`PC chooses ${choices[pcsChoice]}`);
-    // console.log("You lose!");
     var resultA = "You lose!";
   }
   // PLAYER CHOOSE SCISSOR
   if (playerChoice == "scissor" && pcsChoice == 0) {
-    // console.log(`PC chooses ${choices[pcsChoice]}`);
-    // console.log("You lose!");
     var resultA = "You lose!";
   } else if (playerChoice == "scissor" && pcsChoice == 1) {
-    // console.log(`PC chooses ${choices[pcsChoice]}`);
-    // console.log("you win!");
     var resultA = "You win!";
   } else if (playerChoice == "scissor" && pcsChoice == 2) {
-    // console.log(`PC chooses ${choices[pcsChoice]}`);
-    // console.log("TIE!");
     var resultA = "TIE!";
   } else {
     // ???
